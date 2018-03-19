2452eddfca26d065d0b06093da8ab4cb6398782c
Waqar Haider <whaider@bu.edu>
2018-03-18 21:41:07 -0400

    Farm-Analyzer
    EC 500 Project on Farm Analyzer. This application provides following features:-
    
    Stores all media captured in every location of the farm.
    Retrieves the information based on the location.
    Compares media captured in different times.
    Provides APIs or services to integrate into third party applications to upload or download media.

---
+++ app/src/main/java/com/example/waqarhaider/farm_analyzer/FarmInfo.java
@@ -0,0 +1,13 @@
+package com.example.waqarhaider.farm_analyzer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FarmInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farm_info);
+    }
+}
